1a0f01286ad4a77f230b94e7644e2b74795cffe4
Snegirev Anfim <anfim.sn@gmail.com>
2022-11-18 00:15:39 +0400

    Delete task Api and move requests in Api service

--- frontend/src/services/ApiService.ts
+++ frontend/src/services/ApiService.ts
@@ -1,29 +1,69 @@
-import {TaskApi} from './TaskApi'
+import axios from 'axios'
+import {ITask} from '../typings'
+
+type CreateTaskBody = Pick<ITask, 'text' | 'userId'>
+type UpdateTaskBody = Pick<ITask, 'id' | 'text'>
 
 export class ApiService {
-  private static taskApi = new TaskApi()
+  private static url = 'http://localhost:9001/'
 
-  static getAllTasks() {
-    return this.taskApi.getAll()
+  static async getAllTasks() {
+    try {
+      const response = await axios.get<ITask[] | string>(this.url)
+      return response.data
+    } catch (e) {
+      return e as string
+    }
   }
 
-  static getTaskByUserId(userId = 1) {
-    return this.taskApi.findByUserId(userId)
+  static async getTaskByUserId(userId: number) {
+    try {
+      const url = `${this.url}task?userId=${userId}`
+      const response = await axios.get<ITask[] | string>(url)
+      return response.data
+    } catch (e) {
+      return e as string
+    }
   }
 
-  static getTaskById(id = 1) {
-    return this.taskApi.findById(id)
+  static async getTaskById(id: number) {
+    try {
+      const url = `${this.url}task/${id}`
+      const response = await axios.get(url)
+      return response.data
+    } catch (e) {
+      return e as string
+    }
   }
 
-  static createTask({text, userId} = {text: '', userId: 0}) {
-    return this.taskApi.create({text, userId})
+  static async createTask({text, userId}: CreateTaskBody) {
+    if (!text) return 'text is empty'
+    try {
+      const response = await axios.post<ITask | string>(this.url, {text, userId})
+      return response.data
+    } catch (e) {
+      return e as string
+    }
   }
 
-  static updateTask({id, text} = {id: 0, text: ''}) {
-    return this.taskApi.update({id, text})
+  static async updateTask({id, text}: UpdateTaskBody) {
+    if (!text) return 'text is empty'
+    try {
+      const url = `${this.url}task/${id}`
+      const response = await axios.put(url, {text})
+      return response.data
+    } catch (e) {
+      return e as string
+    }
   }
 
-  static deleteTask(id = 0) {
-    return this.taskApi.delete(id)
+  static async deleteTask(id: number) {
+    try {
+      const url = `${this.url}task/${id}`
+      await axios.delete(url)
+      return `${id} task is deleted`
+    } catch (e) {
+      return e as string
+    }
   }
 }
